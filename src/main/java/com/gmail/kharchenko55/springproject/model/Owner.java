9889d436515f32ba8389794a67ae3d7ed70aebe8
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-03-01 00:01:15 +0200

    Added POJOs. Closes #2.

---
+++ src/main/java/com/gmail/kharchenko55/springproject/model/Owner.java
@@ -0,0 +1,4 @@
+package com.gmail.kharchenko55.springproject.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/gmail/kharchenko55/springproject/model/Vet.java
@@ -0,0 +1,4 @@
+package com.gmail.kharchenko55.springproject.model;
+
+public class Vet extends Person {
+}
