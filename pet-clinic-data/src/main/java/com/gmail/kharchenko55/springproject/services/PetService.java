700f0543213600a52bfda47158c15fe6d17eddc0
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-03-18 22:42:10 +0200

    Refactored to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/PetService.java
+++ pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/PetService.java
@@ -2,10 +2,6 @@ package com.gmail.kharchenko55.springproject.services;
 
 import com.gmail.kharchenko55.springproject.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/VetService.java
+++ pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/VetService.java
@@ -2,10 +2,6 @@ package com.gmail.kharchenko55.springproject.services;
 
 import com.gmail.kharchenko55.springproject.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
