ff1a38972d6d2e3f47dfe9ada5b659f985f76067
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-03-19 22:40:15 +0200

    Impl Map Based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.gmail.kharchenko55.springproject.services.map;
+
+import com.gmail.kharchenko55.springproject.model.Pet;
+import com.gmail.kharchenko55.springproject.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
