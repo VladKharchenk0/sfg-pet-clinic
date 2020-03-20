1dd7885faf7e6ad413cc85c30d9ebb6b0bfd5610
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-03-20 11:16:50 +0200

    Added Owner Index page and Controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/com/gmail/kharchenko55/springproject/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package com.gmail.kharchenko55.springproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
