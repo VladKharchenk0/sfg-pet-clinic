b289b7d5d6d025b48ba3eba3ca418ab0a28aa666
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-04-26 23:32:56 +0300

    Added SpecialityMap Impl. Close #31.

---
+++ pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.gmail.kharchenko55.springproject.services;
+
+import com.gmail.kharchenko55.springproject.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package com.gmail.kharchenko55.springproject.services.map;
+
+import com.gmail.kharchenko55.springproject.model.Speciality;
+import com.gmail.kharchenko55.springproject.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        super.findById(aLong);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
