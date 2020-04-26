b03050576d5ed1adf9898fdf9aa690d2e85f7ddb
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-04-26 19:45:46 +0300

    Added PetTypeService. Close #29.

---
+++ pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.gmail.kharchenko55.springproject.services;
+
+import com.gmail.kharchenko55.springproject.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.gmail.kharchenko55.springproject.services.map;
+
+import com.gmail.kharchenko55.springproject.model.PetType;
+import com.gmail.kharchenko55.springproject.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
