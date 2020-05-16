d9db0713ce3194367ebceb024e997965933348b4
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-05-16 23:34:14 +0300

    Added Visit map service. Close #46.

---
+++ pet-clinic-data/src/main/java/com/gmail/kharchenko55/springproject/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.gmail.kharchenko55.springproject.services;
+
+import com.gmail.kharchenko55.springproject.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
